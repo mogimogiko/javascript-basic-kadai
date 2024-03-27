4465530b4562161e819552fd959f58c095954c3f
mogimogiko <moemmmoe66@gmail.com>
2024-03-27 17:14:01 +0900

    記述の順番を変更

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,15 +1,14 @@
 // ランダムに数字を入れる
-var num = 15;
+var num = 5;
 
 // numを出力
 console.log(num)
-// 3の倍数の場合
-if ((num % 3) === 0){
+if ((num % 3 === 0) && (num % 5 === 0)) {
+  console.log('3と5の倍数です'); 
+} else if ((num % 3) === 0){
   console.log('3の倍数です');
 } else if ((num % 5) === 0){// 5の倍数の場合
   console.log('5の倍数です');
-} else if ((num % 3 === 0) && (num % 5 === 0)){// 3と5の倍数の場合
-  console.log('3と5の倍数です');
 } else {// それ以外の場合
   console.log(num);
 }
