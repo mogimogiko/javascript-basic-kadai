6b1b22e111a9b9312ef50d7fcb396767780fc4eb
mogimogiko <moemmmoe66@gmail.com>
2024-03-30 13:53:38 +0900

    while文修正

--- kadai_009/holiday.js
+++ kadai_009/holiday.js
@@ -1,5 +1,13 @@
 // 配列
 const holiday = ['正月','成人の日', '建国記念日','天皇誕生日','春分の日','昭和の日','憲法記念日','みどりの日','こどもの日','海の日','山の日','敬老の日','秋分の日', 'スポーツの日','文化の日','勤労感謝の日']
 
+// for文繰り返し
+for (let i = 0; i < holiday.length; i += 1) {
+  console.log(holiday[i]);
+}
 // while繰り返し
-holiday 
+let i = 0; 
+while (i < holiday.length) {
+  console.log(holiday[i]);
+  i += 1;
+}
