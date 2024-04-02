482a2d6b3d01b6172017e30cc3757f162b9ba9d5
mogimogiko <moemmmoe66@gmail.com>
2024-04-02 17:32:42 +0900

    syuusei

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,6 +1,6 @@
 var today = new Date();
 var year = today.getFullYear();
-var month = today.getMonth();
+var month = today.getMonth() + 1;
 var day = today.getDate();
 
 console.log(year + '年' + month + '月' + day + '日');
